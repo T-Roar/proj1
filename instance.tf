94b1d77261ff542846e325666858d6a7880fc799
T--Roar <dennartempest@aol.com>
2023-11-07 09:09:42 -0700

    initial commit

--- instance.tf
+++ instance.tf
@@ -11,48 +11,26 @@ resource "google_compute_instance" "power_instance" {
 
   network_interface {
     network = "default"
-    access_config {
-      nat_ip = google_compute_address.external_ip.address
-    }
   }
 
   metadata = {
-    ssh-keys       = "${file("${path.module}/id_rsa.pub")}"
     startup-script = templatefile("${path.module}/startup-script.sh", { external_ip = google_compute_address.external_ip.address })
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/id_rsa.pub"
+    destination = "/home/tehila/.ssh/authorized_keys"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "tehila"
+    private_key = file("${path.module}/id_rsa")
+    host        = google_compute_address.external_ip.address
   }
 }
 
 resource "google_compute_address" "external_ip" {
-  name = "power-external-ip"
+  name   = "power-external-ip"
   region = var.gcp_region
-}
-
-resource "google_compute_firewall" "firewalls" {
-  name    = "power-firewall"
-  network = google_compute_network.ntwk.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-4000"]
-  }
-  
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_network" "ntwk" {
-  name = "power-network"
 }
